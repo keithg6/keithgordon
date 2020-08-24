ffeb5a2db87d08e6d9d5326954b4ca8ab46a8e8d
Keith Gordon <keithagordon6@gmail.com>
2020-08-24 02:37:09 -0700

    finished styling

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 
 document.addEventListener('scroll', function (e) {
-    if ((window.pageYOffset + window.innerHeight) > (document.querySelector('#target > div').offsetTop)) {
-        document.getElementById('target').classList.add('slideUp');
-        console.log("here");
+    if ((window.pageYOffset + window.innerHeight) > (document.querySelector('#Summary > div').offsetTop)) {
+        document.getElementById('Summary').classList.add('slideUp');
     }
 });
+
+// document.addEventListener('scroll', function (e) {
+//     if ((window.pageYOffset + window.innerHeight) > (document.querySelector('#Education > div').offsetTop)) {
+//         document.getElementById('Education').classList.add('slideLeft');
+//     }
+// });
+
+// document.addEventListener('scroll', function (e) {
+//     if ((window.pageYOffset + window.innerHeight) > (document.querySelector('#Experience > div').offsetTop)) {
+//         document.getElementById('Experience').classList.add('slideRight');
+//     }
+// });
+
+// document.addEventListener('scroll', function (e) {
+//     if ((window.pageYOffset + window.innerHeight) > (document.querySelector('#Projects > div').offsetTop)) {
+//         document.getElementById('Projects').classList.add('slideUp');
+//     }
+// });
